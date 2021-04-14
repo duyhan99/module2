2be36c1f0bfe447d2fe1edd0d48fbf600848e081
duyhan99 <duyhan1602@gmail.com>
2021-04-14 10:27:07 +0700

    update

---
+++ Circlee/TestCircle.java
@@ -0,0 +1,7 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle(6.0, "blue");
+        System.out.println(circle);
+        System.out.println(circle.getRadius());
+    }
+}
---
+++ Circlee/TestCylinder.java
@@ -0,0 +1,7 @@
+public class TestCylinder {
+    public static void main(String[] args) {
+        Cylinder cy = new Cylinder(6, "blue", 6);
+        System.out.println(cy);
+        System.out.println(cy.getVolume());
+    }
+}
